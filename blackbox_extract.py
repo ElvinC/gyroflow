d6ce7f8b15f1634b177de316ca7fcd60907c8e74
Nikel9O <57232709+Nikel9O@users.noreply.github.com>
2020-09-10 10:37:24 +0200

    blackbox_extract update
    updated using orangebox library from https://github.com/atomgomba/orangebox

--- blackbox_extract.py
+++ blackbox_extract.py
@@ -1,8 +1,37 @@
-# TODO implement code to extract data from betaflight blackbox log
+from orangebox import Parser
+import math
+import numpy as np
 
 class BlackboxExtractor:
     def __init__(self, path):
         print("Opening {}".format(path))
+        self.parser = Parser.load(path)
+        self.n_of_logs = self.parser.reader.log_count
+        self.gyro_scale = self.parser.headers["gyro_scale"] #should be already scaled in the fc
+        self.final_gyro_data = []
 
     def get_gyro_data(self):
-        return "sorry mate, this doesn't work yet"
+        for lg in range(1,self.n_of_logs+1):
+            self.parser.set_log_index(lg)
+            t  = self.parser.field_names.index('time')
+            gx = self.parser.field_names.index('gyroADC[0]')
+            gy = self.parser.field_names.index('gyroADC[1]')
+            gz = self.parser.field_names.index('gyroADC[2]')
+
+            data_frames = []
+            for frame in self.parser.frames():
+                f = [frame.data[t]/1000000,
+                     math.radians(frame.data[gx]),
+                     math.radians(frame.data[gy]),
+                     math.radians(frame.data[gz])]
+                data_frames.append(f)
+
+            self.final_gyro_data.extend(data_frames)    
+        return np.array(self.final_gyro_data)
+
+#testing
+if __name__ == "__main__":
+    bbe = BlackboxExtractor("btfl_all.bbl")
+    gyro_data = bbe.get_gyro_data()
+    print(gyro_data)
+    print(bbe.n_of_logs)
