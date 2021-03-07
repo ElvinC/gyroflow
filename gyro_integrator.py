83bcf97c7fba8c9a30dbd8ecbac0bc2f138c33bb
Marc <marc>
2021-03-08 00:47:09 +0100

    sampletime

--- gyro_integrator.py
+++ gyro_integrator.py
@@ -7,6 +7,8 @@ This module uses gyroscope data to compute quaternion orientations over time
 
 import numpy as np
 import quaternion as quat
+from matplotlib import pyplot as plt
+from scipy import stats
 
 class GyroIntegrator:
     def __init__(self, input_data, time_scaling=1, gyro_scaling=1, zero_out_time=True, initial_orientation=None, acc_data=None):
@@ -57,6 +59,42 @@ class GyroIntegrator:
 
         self.already_integrated = False
 
+    def sampleFrequencyAnalysis(self):
+        timestamps = self.get_raw_data("t")
+        gyro_data = self.get_raw_data("xyz")
+        interarrival = np.diff(timestamps, n=1)
+        w = int(self.gyro_sample_rate/100.0) # aggregate over 1%, e.g., 9 gyro samples for 900Hz/900 samples per second
+        interarrival = np.convolve(interarrival, np.ones(w), 'valid') / w
+        freqs = 1.0/interarrival
+        
+        median = np.median(freqs)
+        mad = stats.median_abs_deviation(freqs)
+        mad_normal = stats.median_abs_deviation(freqs, scale='normal')
+        std = np.std(freqs)
+        
+        print('Computed sample rate is {}'.format(self.gyro_sample_rate))
+        print('Median freq is {}'.format(median))
+        print('Mean freq is {}'.format(np.mean(freqs)))
+        print('Stdev of freqs is {}'.format(std))
+        print('MAD of freqs is {}'.format(mad))
+        print('MAD (normal) of freqs is {}'.format(mad_normal))
+        
+        thresh = mad_normal if mad_normal > std else std
+        thresh = 5*thresh #corresponds to 100% of observations when following normal distribution
+        outlierMask = np.abs(freqs - median) > thresh
+        plt.plot(timestamps, gyro_data)
+        plt.plot(timestamps[:-1-(w-1)], outlierMask*2)
+        plt.show()
+        
+        plt.hist(freqs, bins=300)
+        plt.axvline(x=median+thresh)
+        plt.axvline(x=median-thresh)
+        plt.axvline(x=median, color='green')
+        plt.axvline(x=np.mean(freqs), color='orange')
+        plt.show()
+        
+        
+   
 
     def integrate_all(self):
         """go through each gyro sample and integrate to find orientation
