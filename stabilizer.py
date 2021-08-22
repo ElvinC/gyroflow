722705d18b6e79a2af077df0b90a2510bb354333
Elvin Chen <20195216+ElvinC@users.noreply.github.com>
2021-08-22 19:33:32 +0200

    Minor export code refractor

--- stabilizer.py
+++ stabilizer.py
@@ -1186,7 +1186,7 @@ class Stabilizer:
 
         i = 0
 
-        starttime = time.time()
+        starttime_render = time.time()
 
         # Double press q to stop render
         quit_button = False
@@ -1206,7 +1206,7 @@ class Stabilizer:
 
             if i % 5 == 0:
                 fraction_done = i/num_frames
-                elapsed_time = time.time() - starttime  # in seconds
+                elapsed_time = time.time() - starttime_render  # in seconds
                 est_remain = (elapsed_time) * (1/max(fraction_done, 0.00001) - 1)
                 #print("frame: {}, {}/{} ({}%), ~{} s remaining".format(frame_num, i, num_frames, round(100 * fraction_done,1), round(est_remain)))
 
@@ -1326,7 +1326,7 @@ class Stabilizer:
         out.close()
 
         if audio:
-            time.sleep(1)
+            time.sleep(.5)
             ffmpeg_command = [
                 "-y",
                 "-i",
