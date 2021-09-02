698f0f5e15b88b27ebce4eeda3c5f93f6ef64fd8
Markus Rampp <mail@markusrampp.eu>
2021-09-02 16:07:21 +0200

    remove test file

--- test scripts/sync_suggestion.py
+++
@@ -1,8 +0,0 @@
-import stabilizer
-
-file = r"D:\git\FPV\videos\GH011155.MP4"
-
-stab = stabilizer.GPMFStabilizer(file, r"D:\git\FPV\GoPro_Hero6_2160p_43.json", file, hero=6, fov_scale = 1.6, gyro_lpf_cutoff = -1)
-
-stab.full_auto_sync()
-
